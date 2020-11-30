5a29db32164876fe820f1897a4d5948ccf0339d8
mahmoud atef <mahmoud@pop-os.localdomain>
2020-11-30 21:00:44 +0200

    add login modal sheet in favourite screen

--- lib/view/welcome_screen.dart
+++ lib/view/welcome_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wallpaperapp/view/authentication/register_screen.dart';
 import 'package:wallpaperapp/view/authentication/signIn_screen.dart';
-import 'package:wallpaperapp/view/home_screen/home_screen.dart';
 import 'package:wallpaperapp/viewmodel/home_modelView.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -11,25 +10,11 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  bool _loginScreen = true;
-  void toggleScreen() {
-    setState(() {
-      _loginScreen = !_loginScreen;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return context.watch<HomePageViewModel>().loginScreen
         ? LoginScreen()
         : RegisterScreen();
-
-    // _loginScreen
-    //     ? LoginScreen(
-    //         toggleScreen: toggleScreen,
-    //       )
-    //     : RegisterScreen(
-    //         toggleScreen: toggleScreen,
-    //       );
   }
 }
