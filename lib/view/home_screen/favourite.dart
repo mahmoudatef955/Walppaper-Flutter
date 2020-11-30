af3dab2bf2ab0678e2ba871691979c012a0d7b05
mahmoud atef <mahmoud@pop-os.localdomain>
2020-11-30 19:35:30 +0200

    refactoring

--- lib/view/home_screen/favourite.dart
+++ lib/view/home_screen/favourite.dart
@@ -1,22 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
-import 'package:wallpaperapp/model/favWallpaper.dart';
+import 'package:wallpaperapp/services/webService.dart';
+import 'package:wallpaperapp/viewmodel/home_modelView.dart';
+import 'package:provider/provider.dart';
+import 'detailsScreen.dart';
 
 class Favourite extends StatelessWidget {
-  Box favBox = Hive.box('favBox');
+  Box<String> favoriteImageBox = Hive.box('favBox');
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-        valueListenable: favBox.listenable(),
+        valueListenable: favoriteImageBox.listenable(),
         builder: (context, box, _) {
-          FavWallpapers favWallpapers = box.get('favWallpapers');
+          int count = box.length;
+
           return Container(
             width: MediaQuery.of(context).size.width,
             color: Colors.black12,
-            child:  favWallpapers==null?Center(child: Text('No  Wallpapers'))
-            :Center(child: Text('fav wall')),
+            child: count == 0
+                ? Center(child: Text('No  Wallpapers'))
+                : ImageGridView(data: box.toMap()),
           );
         });
+  }
+}
+
+class ImageGridView extends StatefulWidget {
+  const ImageGridView({this.data});
+
+  final Map data;
+
+  @override
+  _ImageGridViewState createState() => _ImageGridViewState();
+}
+
+class _ImageGridViewState extends State<ImageGridView> {
+  int currentPageNumber = 1;
+  WebService webService = WebService();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var keys = widget.data.keys.toList();
+
+    return GridView.builder(
+      itemCount: widget.data.keys.length,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (context, index) {
+        return BuildGridCard(
+          imageid: keys[index],
+          imageurl: widget.data[keys[index]],
+        );
+      },
+    );
+  }
+}
+
+class BuildGridCard extends StatelessWidget {
+  final String imageid;
+  final String imageurl;
+
+  const BuildGridCard({Key key, this.imageid, this.imageurl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _homeScreenProvider = context.watch<HomePageViewModel>();
+
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DetailsScreen(imageid)),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Stack(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            child: Image.network(
+              imageurl,
+              fit: BoxFit.fill,
+              height: 600,
+              width: 200,
+            ),
+          ),
+          Positioned(
+            right: 6,
+            bottom: 6,
+            child: GestureDetector(
+              onTap: () =>
+                  _homeScreenProvider.onFavoritePress(imageid, imageurl),
+              child: getIcon(imageid, _homeScreenProvider),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget getIcon(String index, _homeScreenProvider) {
+    if (_homeScreenProvider.favoriteImageBox.containsKey(index)) {
+      return Icon(Icons.favorite, color: Colors.red);
+    }
+    return Icon(Icons.favorite_border);
   }
 }
--- lib/view/home_screen/popular.dart
+++ lib/view/home_screen/popular.dart
@@ -1,34 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:wallpaperapp/services/webService.dart';
-import 'package:wallpaperapp/view/home_screen/detailsScreen.dart';
+import 'package:wallpaperapp/viewmodel/home_modelView.dart';
+import 'image_gridView.dart';
 
 enum MovieLoadMoreStatus { LOADING, STABLE }
 
-class PopularScreen extends StatefulWidget {
-  @override
-  _PopularScreenState createState() => _PopularScreenState();
-}
-
-class _PopularScreenState extends State<PopularScreen> {
+class PopularScreen extends StatelessWidget {
   List data;
-  bool _loading = true;
-  int currentPageNumber;
-  WebService webService = WebService();
+  int currentPageNumber = 1;
+  String _orderBy = 'popular';
   MovieLoadMoreStatus loadMoreStatus = MovieLoadMoreStatus.STABLE;
 
   @override
-  void initState() {
-    super.initState();
-    currentPageNumber = 1;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final _homeScreenProvider = context.watch<HomePageViewModel>();
     return Scaffold(
         backgroundColor: Colors.grey[900],
         body: FutureBuilder(
-            future: webService.fetchImages(currentPageNumber),
+            future:
+                _homeScreenProvider.fetchImages(currentPageNumber, _orderBy),
             builder: (context, snapshots) {
               if (snapshots.hasError) return Text("Error Occurred");
               switch (snapshots.connectionState) {
@@ -37,125 +27,11 @@ class _PopularScreenState extends State<PopularScreen> {
                 case ConnectionState.done:
                   print('-----------------done-----------------');
                   data = snapshots.data;
-                  return ImageGridView(data: data);
+                  return ImageGridView(
+                    data: data,
+                    orderBy: _orderBy,
+                  );
               }
             }));
-  }
-}
-
-class ImageGridView extends StatefulWidget {
-  const ImageGridView({
-    Key key,
-    @required this.data,
-  }) : super(key: key);
-
-  final List data;
-
-  @override
-  _ImageGridViewState createState() => _ImageGridViewState();
-}
-
-class _ImageGridViewState extends State<ImageGridView> {
-  int currentPageNumber = 1;
-  WebService webService = WebService();
-  final ScrollController scrollController = new ScrollController();
-
-  MovieLoadMoreStatus loadMoreStatus = MovieLoadMoreStatus.STABLE;
-
-  @override
-  void dispose() {
-    scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return NotificationListener(
-      onNotification: onNotification,
-      child: GridView.builder(
-        controller: scrollController,
-        itemCount: widget.data.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return BuildGridCard(
-            imageData: widget.data[index],
-          );
-        },
-      ),
-    );
-  }
-
-  bool onNotification(ScrollNotification notification) {
-    if (notification is ScrollUpdateNotification) {
-      if (scrollController.position.maxScrollExtent > scrollController.offset &&
-          scrollController.position.maxScrollExtent - scrollController.offset <=
-              50) {
-        if (loadMoreStatus != null &&
-            loadMoreStatus == MovieLoadMoreStatus.STABLE) {
-          loadMoreStatus = MovieLoadMoreStatus.LOADING;
-          currentPageNumber = currentPageNumber + 1;
-          print('-----------Load more 1111-----------------');
-          webService.fetchMoreImages(currentPageNumber).then((value) {
-            print('-----------Load more 22222-----------------');
-            loadMoreStatus = MovieLoadMoreStatus.STABLE;
-            setState(() {
-              widget.data.addAll(value);
-            });
-          });
-          loadMoreStatus = MovieLoadMoreStatus.STABLE;
-        }
-      }
-    }
-    return true;
-  }
-}
-
-class BuildGridCard extends StatelessWidget {
-  const BuildGridCard({
-    Key key,
-    @required this.imageData,
-  }) : super(key: key);
-
-  final imageData;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => DetailsScreen(imageData['id'])),
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Stack(children: [
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            child: Image.network(
-              imageData['urls']['small'],
-              fit: BoxFit.fill,
-              height: 600,
-              width: 200,
-            ),
-          ),
-          Positioned(
-            right: 6,
-            bottom: 6,
-            child: GestureDetector(
-              onTap: () {
-                print('favourite');
-              },
-              child: Icon(
-                Icons.favorite_border,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ]),
-      ),
-    );
   }
 }
